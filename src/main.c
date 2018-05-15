dd5f5876d80a3446ff0b2e4f1a092f2651197ecd
Unknown <romain.guillot@etu.univ-grenoble-alpes.fr>
2018-05-15 11:39:31 +0200

    conversion_tableau_liste + structures

--- src/main.c
+++ src/main.c
@@ -4,6 +4,46 @@
 #include <string.h>
 
 
+pnoeud_t creer_noeud(int poids) {
+    pnoeud_t noeud = malloc(sizeof(noeud_t));
+    noeud->poids = poids;
+    return noeud;
+}
+
+void ajouter_queue(pnoeud_t noeud, pliste_t liste){
+    liste->queue->suiv = noeud;
+    liste->queue = noeud;
+}
+
+void conversion_tableau_liste(int* occurence, pliste_t liste) {
+    int i = 0;
+    while(occurence[i] == 0) {
+        i++;
+    }
+    pnoeud_t noeud = creer_noeud(occurence[i]);
+    liste->tete->noeud = noeud;
+    pnoeud_t noeud_crt = tete->noeud;
+
+    for( ; i<256; i++) {
+        if(occurence[i] != 0) {
+            pnoeud_t noeud = creer_noeud(occurence[i]);
+            noeud_crt->suiv = noeud;
+            noeud_crt = noeud;
+        }
+    }
+    liste->queue = noeud_crt;
+}
+
+
+
+
+
+pnoeud_t creer_arbre_canonique(int* occurence) {
+    pliste_t liste = malloc(sizeof(liste_t));
+    conversion_tableau_liste(occurence, liste);
+}
+
+
 int main(int argc, char const *argv[]) {
   /* code */
   return 0;
---
+++ src/main.h
@@ -0,0 +1,19 @@
+#ifndef COMPRESSION_H
+#define COMPRESSION_H
+
+typedef struct n {
+    struct n *parent;
+    struct n *fgauche;
+    struct n *fdroit;
+    int poids;
+    char c;
+    struct n *suiv;
+} noeud_t, *pnoeud_t;
+
+typedef struct {
+    pnoeud_t tete;
+    pnoeud_t queue;
+} liste_t, pliste_t;
+
+
+#endif
