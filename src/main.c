d03da3b9fdbc6743dbf9e9e5ea5633d8e44dfc7f
Abyssion <alexis@lattard.fr>
2018-05-15 14:46:26 +0200

    début modif

--- src/main.c
+++ src/main.c
@@ -4,27 +4,8 @@
 #include <string.h>
 #include "main.h"
 
-int* table_pourcentage_huffman(char const* file_name){ //Recoit le nom du fichier a compresser (argv[1]) et renvoie un tableau a 256 cases des occurences des caractères
-  FILE* f = fopen(file_name, "r");
-  if (!f) {
-    printf("Ouverture du fichier impossible. Abandon.\n");
-    exit(0);
-  }
-  int* table = malloc(256 * sizeof(int));
-  for (int i = 0; i < 256; i++) {
-    table[i] = 0;
-  }
-  char c;
-  while ((c = fgetc(f)) != EOF) {
-    table[c]++;
-  }
-  fclose(f);
-
-  return table;
-}
-
 table_l_code* creer_table_arbre(pnoeud_t racine){
-  table_l_code* tab = malloc(256*sizeof(4*uint64+int+char));
+  table_l_code* tab = malloc(256*sizeof(table_l_code));
   pnoeud_t bl = racine;
   save = tab;
   int i=0;
