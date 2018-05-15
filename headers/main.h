3c46346d6b66ffda06e62d894eb31d1cd96e0093
Hoel <jalmin.hoelle@gmail.com>
2018-05-15 15:44:47 +0200

    Auto stash before merge of "creer_table_arbre" and "origin/creer_table_arbre"

--- headers/main.h
+++ headers/main.h
@@ -4,10 +4,7 @@
 typedef struct {
     char symbole;
     int longueur;
-    uint64_t code4;
-    uint64_t code3;
-    uint64_t code2;
-    uint64_t code1;
+    uint64_t* code;
 } table_l_code;
  
 #endif
--- src/main.c
+++ src/main.c
@@ -4,29 +4,51 @@
 #include <string.h>
 #include "main.h"
 
+int* table_pourcentage_huffman(char const* file_name){ //Recoit le nom du fichier a compresser (argv[1]) et renvoie un tableau a 256 cases des occurences des caractères
+  FILE* f = fopen(file_name, "r");
+  if (!f) {
+    printf("Ouverture du fichier impossible. Abandon.\n");
+    exit(0);
+  }
+  int* table = malloc(256 * sizeof(int));
+  for (int i = 0; i < 256; i++) {
+    table[i] = 0;
+  }
+  char c;
+  while ((c = fgetc(f)) != EOF) {
+    table[c]++;
+  }
+  fclose(f);
+
+  return table;
+}
+
 table_l_code* creer_table_arbre(pnoeud_t racine){
-  table_l_code* tab = malloc(256*sizeof(table_l_code));
+  table_l_code* tab = malloc(256*sizeof(4*uint64+int+char));
   pnoeud_t bl = racine;
   save = tab;
   int i=0;
-  while(bl!=NULL){
-    while((bl->fgauche!=NULL)&&(bl->fdroit!=NULL)){
-      if(bl->fdroit != NULL){
-        bl = bl->fdroit;
+  while(racine!=NULL){
+    while((temp->fgauche!=NULL)&&(temp->fdroit!=NULL)){
+      if(temp->fdroit != NULL){
+        temp = temp->fdroit;
       }
-      else if (bl->fgauche!=NULL){
-        bl = bl->fgauche;
+      else if (temp->fgauche!=NULL){
+        temp = temp->fgauche;
       }
       i++;
     }
-    tab[i]->symbole=bl->c;
+    tab[i]->symbole=temp->c;
     //et pour le reste idk
   }
-  return save;
+  return tab;
 }
 
-
+*/
 int main(int argc, char const *argv[]) {
-
+  int* t = table_pourcentage_huffman(argv[1]);
+ // for (int i=0; i<256;i++){
+    
+  //}
   return 0;
 }
