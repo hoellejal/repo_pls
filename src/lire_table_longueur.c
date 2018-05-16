064ccf2987a63b4defcd96baa5eef116dc709fc9
Hoel <jalmin.hoelle@gmail.com>
2018-05-16 11:27:46 +0200

    Fini yooo

--- src/lire_table_longueur.c
+++ src/lire_table_longueur.c
@@ -35,24 +35,6 @@ ptable_long lire_table_longueur(const char* file_name){
   return t;
 }
 
-int* table_fichier(char const* file_name){
-  FILE* f = fopen(file_name, "r");
-  if (!f) {
-    printf("Ouverture du fichier impossible. Abandon.\n");
-    exit(0);
-  }
-
-  int c, i=0,j;
-  c=(fgetc(f))-'0';
-  if (c==EOF){
-    printf("Fichier vide. Abandon.\n");
-    exit(0);
-  }
-
-  while(((j = fgetc(f)) != EOF) && i<c*2) // on ne relis pas l'entête, table des longueurs
-  
-}
-
 int main(int argc, char const *argv[]){
   ptable_long y=lire_fichier(argv[1]);
 }
