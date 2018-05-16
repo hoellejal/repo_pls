0ab835a9bd6e58e2c1abb293c0855610416c45e6
Hoel <jalmin.hoelle@gmail.com>
2018-05-16 11:39:46 +0200

    ayyyyy

---
+++ src/compression.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+
+int ecrire_fichier_compresse(table_longueur){
+  
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
