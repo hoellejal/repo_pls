f9f5b1cf6e905ed1857e415b46cab4bf2fd95fb3
Hoel <jalmin.hoelle@gmail.com>
2018-05-16 09:50:46 +0200

    blh

--- src/compression.c
+++ src/compression.c
@@ -14,12 +14,17 @@ typedef struct {
   uint8_t longueur;
 } codage_canonique_t , *pcodage_canonique_t;
 
-int ecrire_fichier_compresse(pcodage_t){
+int ecrire_fichier_compresse(pcodage_t, const char* file_name){
   //Lit le fichier initial et compresse dans un autre fichier grâce à la table des longueurs.
+  FILE* f = fopen(file_name, "r");
+  if (!f) {
+    printf("Ouverture du fichier impossible. Abandon.\n");
+    exit(0);
+  }
+  
   
   return 1;
 }
-
 
 int main(int argc, char const *argv[]) {
 
